d0baf39e047961744ad27cf4deac259e60212dac
hxl <903133815@qq.com>
2019-08-26 15:39:34 +0800

    账单日志可编辑

--- 花支任务小程序/pages/logs/logs.js
+++ 花支任务小程序/pages/logs/logs.js
@@ -13,32 +13,170 @@ Page({
     //今日账单
     nowDayLogs:[],
     nowDaySigns:[],
+
+    input_note:"",
+    showChangeView:false,
+    changeId:"", //前缀 n今日 h历史
+    animation:"",
   },
 
-  //点击 询问是否删除
-  click:function(e){
-    var tempNowDayTaskLogs = wx.getStorageSync('nowDayLogs') || []
-    var msg = tempNowDayTaskLogs[e.target.id].substring(1)
+  onLoad: function () {
+    this.animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'linear',
+    })
+
+    this.animation.opacity(0).step({ duration: 0 }).opacity(1).step({ duration: 200 })
+    this.setData({
+      animation: this.animation.export(),
+    })
+  },
+
+  //修改功能
+  goChange: function (e) {
+    //console.log(e.detail.value["input_note"])
+
+    var nowDayLogs = wx.getStorageSync('nowDayLogs') || []
+    var logs = wx.getStorageSync('logs') || []
+
+    var str = e.detail.value["input_note"]
+    var profix = this.data.changeId.substring(0, 1)
+    var id = this.data.changeId.substring(1)
+
+    if (profix == 'n') {
+      var temp = nowDayLogs[id].split(":")
+      temp[3]=str
+      nowDayLogs[id] = temp.join(":")
+
+      wx.setStorageSync('nowDayLogs', nowDayLogs)
+    }
+    else {
+      var temp = logs[id].split(":")
+      temp[3] = str
+      logs[id] = temp.join(":")
+
+      wx.setStorageSync('logs', logs)
+    }
+
+    this.animation.opacity(0).step({ duration: 200 })
+    this.setData({
+      animation: this.animation.export(),
+    })
+
+    var that = this
+    setTimeout(function () {
+      that.setData({
+        showChangeView: false
+      })
+
+      wx.showToast({
+        icon: 'none',
+        title: '修改成功',
+        duration: 1000,
+      })
+    }, 200)
+
+    this.onShow()
+  },
+
+  //删除功能
+  delete:function(){
+    var that = this
+
+    var nowDayLogs = wx.getStorageSync('nowDayLogs') || []
+    var logs = wx.getStorageSync('logs') || []
+
+    var profix = that.data.changeId.substring(0, 1)
+    var id = that.data.changeId.substring(1)
+
+    var msg = ""
+    if (profix == 'n') 
+      msg = nowDayLogs[id]
+    else 
+      msg = logs[id]
+
     var that = this
     wx.showModal({
-      title: '删除记录',
-      content: msg,
-      confirmText:"yes",
-      cancelText:"no",
+      title: '删除',
+      content: msg.substring(1)+' ?',
+      confirmText:'yes',
+      cancelText:'no',
       success:function(res){
         if(res.confirm){
-          tempNowDayTaskLogs.splice(e.target.id,1)
-          wx.setStorageSync('nowDayLogs', tempNowDayTaskLogs)
+          if (profix == 'n') {
+            nowDayLogs.splice(id, 1)
+            wx.setStorageSync('nowDayLogs', nowDayLogs)
+          }
+          else {
+            logs.splice(id, 1)
+            wx.setStorageSync('logs', logs)
+          }
+
+          that.animation.opacity(0).step({ duration: 200 })
+          that.setData({
+            animation: that.animation.export(),
+          })
+
+          setTimeout(function () {
+            that.setData({
+              showChangeView: false
+            })
+          }, 200)
+
           wx.showToast({
-            icon:'none',
+            icon: 'none',
             title: '删除成功',
-            duration:1000,
+            duration: 1000,
           })
 
           that.onShow()
         }
       }
     })
+  },
+
+  //关闭更改视图
+  closeChangeView:function(){
+    this.animation.opacity(0).step({ duration: 200 })
+    this.setData({
+      animation: this.animation.export(),
+    })
+
+    var that = this
+    setTimeout(function () {
+      that.setData({
+        showChangeView: false
+      })
+    }, 200)
+  },
+
+  //显示更改窗口 //nh id前缀
+  click: function (e) {
+    var nowDayLogs = wx.getStorageSync('nowDayLogs') || []
+    var logs = wx.getStorageSync('logs') || []
+    
+    var profix = e.target.id.substring(0,1)
+    var id = e.target.id.substring(1)
+    var tempInputNote = ""
+    
+    if(profix=='n'){
+      tempInputNote = nowDayLogs[id]
+    }
+    else
+      tempInputNote = logs[id]
+
+    this.setData({
+      input_note:tempInputNote.split(":")[3]||"",
+      changeId:e.target.id,
+      showChangeView: true
+    })
+
+    this.animation.opacity(1).step({ duration: 200 })
+    this.setData({
+      animation: this.animation.export(),
+    })
+
+    this.onShow()
   },
 
   //输出日志
