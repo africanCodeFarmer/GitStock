dab04eeff7330acd9ab1eab411cf6a2f20ef4f6b
hxl <903133815@qq.com>
2019-08-29 10:39:22 +0800

    优化自动聚焦

--- 花支任务小程序/pages/study/study.js
+++ 花支任务小程序/pages/study/study.js
@@ -1,2 +1,18 @@
 Page({
+  // data:{
+  //   latitude:0.0,
+  //   longitude:0.0,
+  // },
+
+  // onShow(){
+  //   var that = this
+  //   wx.getLocation({
+  //     success: function(res) {
+  //       that.setData({
+  //         latitude: res.latitude,
+  //         longitude: res.longitude,
+  //       })
+  //     },
+  //   })
+  // }
 })
