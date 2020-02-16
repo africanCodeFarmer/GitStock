a0eebb7a423a5120e012bf7ba036188b800ff2df
hxl <903133815@qq.com>
2020-02-16 20:32:42 +0800

    更新转账功能

--- 开发中备份/miniprogram-1/miniprogram/pages/spend/spend.js
+++ 开发中备份/miniprogram-1/miniprogram/pages/spend/spend.js
@@ -20,7 +20,88 @@ Page({
     go_spend_sign:"", //+-符号
     input_value_comment_error:"",
 
-    spendLogs:[]
+    spendLogs:[],
+
+    show_money_transfer_popup:false,
+    picker_columns:[],
+    transfer_error:"",
+    transfer_value:null,
+    transfer_value_error:"",
+  },
+  onClick_transfer_block:function(e){
+    var value = e.target.dataset.value
+    this.setData({
+      transfer_value:value,
+    })
+  },
+  onChange_transfer_value:function(e){
+    this.setData({transfer_value:e.detail})
+  },
+  go_transfer:function(){
+    var picker = this.selectComponent("#transfer_picker");
+    var values = picker.getValues()
+    var fromName = values[0]
+    var toName = values[1]
+
+    if(fromName == toName){
+      this.setData({transfer_error:"请不要自己转账给自己"})
+      return;
+    }
+    if(this.data.transfer_value==null){
+      this.setData({transfer_value_error:"请输入金额"})
+      return;
+    }
+
+    var fromStock = this.getStock_useName(fromName)
+    var toStock = this.getStock_useName(toName)
+    fromStock.value = parseFloat(fromStock.value)-parseFloat(this.data.transfer_value)
+    toStock.value = parseFloat(toStock.value)+parseFloat(this.data.transfer_value)
+    this.updateStockFromStocks(fromStock)
+    this.updateStockFromStocks(toStock)
+
+    //写日志操作
+    var time = util.formatTime(new Date()).split(' ')[0]
+    var detail_time = util.formatTime(new Date()).split(' ')[1]
+    var spendLogs = this.data.spendLogs
+    if(!this.checkTimeExistSpendLogs(time)) //时间不存在
+      spendLogs.unshift({"time":time,"datas":[]})
+    var spendLog = this.use_time_getSpendLog(time)
+    var log = {
+      "id":spendLog.datas.length>0?spendLog.datas[0].id+1:1,
+      "detail_time":detail_time,
+      "name":fromName,
+      "value":this.data.transfer_value,
+      "comment":"转账给"+toName,
+      "spend_type":null,
+      "operate":'-'
+    }
+    spendLog.datas.unshift(log)
+    var log = {
+      "id":spendLog.datas.length>0?spendLog.datas[0].id+1:1,
+      "detail_time":detail_time,
+      "name":toName,
+      "value":this.data.transfer_value,
+      "comment":"从"+fromName+"收款",
+      "spend_type":null,
+      "operate":'+'
+    }
+    spendLog.datas.unshift(log)
+    this.update_spendLogs(spendLog) //更新转账日志
+
+    this.setData({
+      transfer_error:"",
+      transfer_value_error:"",
+      transfer_value:null,
+      show_money_transfer_popup:false,
+    })
+  },
+  onChange_picker(event) {
+  },
+  transferMoney:function(){
+    this.setData({show_money_transfer_popup:true})
+  },
+  onClose_money_transfer_popup:function(){
+    this.setData({show_money_transfer_popup:false})
   },
   go_spend:function(){
     var str = this.data.input_value_comment
@@ -128,6 +209,12 @@ Page({
     var spend_type = this.data.spend_types[index]
     this.setData({spend_type:spend_type.text})
   },
+  getStock_useName(name){
+    var stocks = this.data.stocks
+    for(var i in stocks)
+      if(stocks[i].name == name)
+        return stocks[i]
+  },
   getStock:function(id){
     var stocks = this.data.stocks
     for(var i in stocks)
@@ -168,7 +255,12 @@ Page({
     var money_blocks = wx.getStorageSync('blocks') || []
     var spend_types = wx.getStorageSync('types') || []
     var spendLogs = wx.getStorageSync('spendLogs') || []
-    
+
+    //转账窗口
+    var pickers = []
+    for(var i in stocks)
+      pickers.push(stocks[i].name)
+
     this.setData({
       stocks:stocks,
       comments:comments,
@@ -176,6 +268,7 @@ Page({
       spend_types:spend_types,
       spend_type:spend_types.length>0?spend_types[0].text:"",
       spendLogs:spendLogs,
+      picker_columns:[{values:pickers},{values:pickers}],
     })
   },
   stock_setting:function(){
