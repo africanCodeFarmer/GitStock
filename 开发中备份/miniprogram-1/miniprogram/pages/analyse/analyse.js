c64eea00222fd021ab4bc244a86482795c81a92e
hxl <903133815@qq.com>
2020-02-24 18:02:59 +0800

    新增分析页清除数据按钮

--- 开发中备份/miniprogram-1/miniprogram/pages/analyse/analyse.js
+++ 开发中备份/miniprogram-1/miniprogram/pages/analyse/analyse.js
@@ -1,7 +1,24 @@
 const app = getApp()
+import Dialog from '@vant/weapp/dialog/dialog';
 
 Page({
   onShow:function(){
-    this.getTabBar().init();
-  }
+    this.getTabBar().init()
+  },
+  onClick_clear:function(){
+    Dialog.confirm({
+      title: '清空',
+      message: '删除花支日志所有数据!?'
+    }).then(() => {
+      // on confirm
+      wx.removeStorageSync('spendLogs')
+
+      wx.showToast({
+        icon:'none',
+        title: '已清空花支日志',
+      })
+    }).catch(() => {
+      // on cancel
+    });
+  },
 })
