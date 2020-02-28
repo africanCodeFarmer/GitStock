1f99f0f2e70fae9ad95c7043979bd8dbd77b2660
hxl <903133815@qq.com>
2020-02-28 23:39:18 +0800

    添加清除所有数据按钮

--- 开发中备份/miniprogram-1/miniprogram/pages/home/home.js
+++ 开发中备份/miniprogram-1/miniprogram/pages/home/home.js
@@ -5,7 +5,23 @@ Page({
   onShow:function(){
     this.getTabBar().init()
   },
-  onClick_clear:function(){
+  onClick_clearAll:function(){
+    Dialog.confirm({
+      title: '清空',
+      message: '删除所有数据!?'
+    }).then(() => {
+      // on confirm
+      wx.clearStorageSync()
+
+      wx.showToast({
+        icon:'none',
+        title: '已清空所有数据',
+      })
+    }).catch(() => {
+      // on cancel
+    });
+  },
+  onClick_clear_spendLogs:function(){
     Dialog.confirm({
       title: '清空',
       message: '删除花支日志所有数据!?'
