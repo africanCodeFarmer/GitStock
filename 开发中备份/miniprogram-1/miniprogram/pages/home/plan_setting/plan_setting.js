1703e51f21b320922840814ef1e1438c06688b9a
hxl <903133815@qq.com>
2020-03-20 22:23:13 +0800

    删除全部文件

--- 开发中备份/miniprogram-1/miniprogram/pages/home/plan_setting/plan_setting.js
+++
@@ -1,8 +0,0 @@
-import Dialog from '@vant/weapp/dialog/dialog';
-
-Page({
-  data: {
-    custom_data:{"backText":"主页","content":"设置"}
-  },
-  
-})
--- 开发中备份/miniprogram-1/miniprogram/pages/spend/target_setting/target_setting.js
+++
@@ -1,5 +0,0 @@
-Page({
-  data: {
-    custom_data:{"backText":"花支","content":"设置"}
-  },
-})
